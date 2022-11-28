e043c97e46c4d0ddfc5a690381ad06753cac02e0
Shiv Gandhi <shiv-gandhi@uiowa.edu>
2022-11-28 15:42:29 -0600

    add home page

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Team Shock</h1>
+       <p>Team 11</p>
+      <Link to="/index">Meet The Members</Link>
+    </main>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default HomePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-
-
+import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <main>
-      <h1>Senior Design Lab3</h1>
+      <h1>Team Members</h1>
       <Link to="/ryan">Ryan</Link>
-      <p> </p>
+      <p>Electrical Engineering</p>
       {/*<Link to="/about2">About 2</Link>*/}
       <Link to="/weston">Weston</Link>
       <p> </p>
