0246a38300d673b4df1dd542e8cf9aff67d430cb
Shiv Gandhi <shiv-gandhi@uiowa.edu>
2022-11-28 13:46:40 -0600

    add pages for everyone

---
+++ src/pages/shiv.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage3 = () => {
+  return (
+    <main>
+      <h1>Shiv</h1>
+      <Link to="/">Back</Link>
+      <p>Shiv's page.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>Member 1</title>
+
+export default AboutPage3
