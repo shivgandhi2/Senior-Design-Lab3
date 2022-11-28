d6a1f26e9f58963fa7276851d09833fb7b384d03
Shiv Gandhi <shiv-gandhi@uiowa.edu>
2022-11-28 15:54:36 -0600

    switch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 //import Layout from '../components/layout'
-const HomePage = () => {
+const IndexPage = () => {
   return (
     <main>
       <h1>Team Shock</h1>
        <p>Team 11</p>
-      <Link to="/index">Meet The Members</Link>
+      <Link to="/home">Meet The Members</Link>
     </main>
   )
 }
 
 export const Head = () => <title>Home Page</title>
 
-export default HomePage
+export default IndexPage
