ef7b4843d5c1caa94ee018ec3922ef5854f9e08c
Noah <79220230+nabushanab@users.noreply.github.com>
2022-11-29 19:57:25 -0600

    Testing my page

--- src/pages/noah.js
+++ src/pages/noah.js
@@ -7,6 +7,9 @@ const AboutPage2 = () => {
       <h1>Noah</h1>
       <Link to="/home">Back</Link>
       <p>Noah's page.</p>
+      <p>Hello, I am an electrical engineering student at UIowa with a focus on computer technology and embedded systems.
+        I am originally from Plainfield, Illinois. In my spare time I like to play chess and hangout with friends.
+      </p>
     </main>
   )
 }
