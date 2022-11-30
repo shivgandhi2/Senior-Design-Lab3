a406679434372461ec938973245eefb7753925f1
Shiv Gandhi <shiv-gandhi@uiowa.edu>
2022-11-29 23:20:58 -0600

    testing image2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import { graphql } from "gatsby"
+
+{/*}
 export default function IndexPage()
 {
   return <div>
@@ -14,23 +16,22 @@ export default function IndexPage()
     />
     </div>
 }
-
+*/}
 //import Layout from '../components/layout'
-{/*
-const IndexPage = () => {
-  return (
+
+const IndexPage = () => (
     <main>
       <h1>Team Shock</h1>
        <p>Team 11</p>
       <Link to="/home">Meet The Members</Link>
       <StaticImage
-      src="../images/Team Shock Logo.png"
+      src="../images/Team Shock Logo.PNG"
       width={600}
       alt="Team Logo"
       />
     </main>
   )
-}
+
 
 //export function Logo() {
 //  return <StaticImage src="../images/Team Shock Logo.PNG" alt="Team Logo" />
@@ -39,4 +40,3 @@ const IndexPage = () => {
 export const Head = () => <title>Home Page</title>
 
 export default IndexPage
-*/}
