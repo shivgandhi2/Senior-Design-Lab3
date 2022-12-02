9f6939b4c74e27243e4237474734e2a276d56adf
WestonYohe <52698313+WestonYohe@users.noreply.github.com>
2022-12-01 19:45:25 -0600

    does it still work?

--- src/pages/weston.js
+++ src/pages/weston.js
@@ -11,7 +11,7 @@ const AboutPage1 = () => {
     
     <main>
       
-      <h1 style={{color: "Aqua"}}>Weston</h1>
+      <h1 style={{color: "blue"}}>Weston</h1>
        
       
       <p>My name is Weston Yohe and I am currently a senior student in the electrical engineering program 
