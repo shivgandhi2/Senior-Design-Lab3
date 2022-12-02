e1d55f126d381671af5cef97d6961a6c2e40aaa4
WestonYohe <52698313+WestonYohe@users.noreply.github.com>
2022-12-01 21:19:48 -0600

    fixed

--- src/pages/weston.js
+++ src/pages/weston.js
@@ -33,7 +33,7 @@ const AboutPage1 = () => {
       
        
       
-      <p>My name is Weston Yohe and I am currently a senior student in the electrical engineering program 
+      <p style ={bodyStyle}>My name is Weston Yohe and I am currently a senior student in the electrical engineering program 
         at the University of Iowa. My focus is on control systems. After high school, one of my jobs was working 
         in a manufacturing plant as a control room operator. This experience plus fixing/restoring an old motorcycle
          is what made me interested in pursuing a career in electrical engineering. When I am not busy with schoolwork,
