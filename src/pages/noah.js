391a9f1d60d7b4562a468950e9eb55e171a69fb8
Noah Abu-Shanab <nabushanab@uiowa.edu>
2022-12-02 11:11:14 -0600

    Update noah.js

--- src/pages/noah.js
+++ src/pages/noah.js
@@ -31,26 +31,26 @@ const AboutPage2 = () => {
         In the past I used to make music and even produced an album on spotify and apple music.
       </p >
 
-      <p style={pictureSyle}></p>
-
-      <StaticImage
-    src="../images/noah-portrait.jpg"
-    width = {500}
-    alt = "Portrait from florida" 
-    />
-
+      <p style={pictureSyle}>
+          <StaticImage
+      src="../images/noah-portrait.png"
+      width = {400}
+      alt="Portrait from florida"
+      />
+      </p>
+      
       <p>ECE</p>
       <p>As an electrical computer engineer, I enjoy the design and maintanence of computers. When I was younger
         I used to create small circuits and this is what initially got me interested in electrical engineering.
       </p>
 
-      <p style={pictureSyle}></p>
-
-      <StaticImage
-    src="../images/noah-computer.png"
-    width = {500}
-    alt = "computer picture" 
-    />
+      <p style={pictureSyle}>
+          <StaticImage
+      src="../images/noah-computer.png"
+      width = {400}
+      alt="computer picture"
+      />
+      </p>
 
       <p>FUTURE</p>
       <p>In the future, I want to get a job working with embedded systems. My focus area in school revolved around computer engineering, and embedded systems is a strong
@@ -58,13 +58,14 @@ const AboutPage2 = () => {
         an engineer, but working as an IT technician would also be an interesting job. I would like to 
       </p>
 
-      <p style={pictureSyle}></p>
 
-      <StaticImage
-    src="../images/noah-micro.jpg"
-    width = {500}
-    alt = "microcontroller picture" 
-    />  
+      <p style={pictureSyle}>
+          <StaticImage
+      src="../images/noah-micro.jpg"
+      width = {400}
+      alt="microcontroller picture"
+      />
+      </p>
 
     <p> </p>
     <Link to="/home">Back</Link>
